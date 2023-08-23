b8211247cc741e0361c0d3f9b811e6dee4074290
Illia <ilyusha.drozd.2016@gmail.com>
2023-08-23 16:53:09 +0300

    init project for testing

---
+++ build/index.cs
@@ -0,0 +1,119 @@
+@import url("https://fonts.googleapis.com/css2?family=Fira+Sans+Extra+Condensed:wght@700&display=swap");
+*,
+*::before,
+*::after {
+  padding: 0;
+  margin: 0;
+  border: 0;
+  outline: 0;
+  box-sizing: border-box;
+  font-size: 100%;
+  font-weight: inherit;
+  font-style: inherit;
+  font-family: inherit;
+  vertical-align: baseline;
+  box-sizing: border-box;
+  position: relative;
+}
+
+:focus {
+  outline: 0;
+}
+
+a {
+  text-decoration: none;
+  color: inherit;
+}
+
+ul,
+ol,
+li {
+  list-style: none;
+}
+
+img {
+  vertical-align: top;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-weight: inherit;
+  font-size: inherit;
+}
+
+html,
+body {
+  height: 100%;
+  line-height: 1.4;
+  color: black;
+  scroll-behavior: smooth;
+}
+
+html {
+  overflow-x: hidden;
+}
+
+button,
+input {
+  font-weight: inherit;
+  font-size: inherit;
+  line-height: inherit;
+  color: inherit;
+  font-family: inherit;
+}
+
+button {
+  cursor: pointer;
+  background-color: inherit;
+}
+
+table {
+  border-collapse: separate;
+  border-spacing: 0;
+}
+
+caption,
+th,
+td {
+  text-align: left;
+  font-weight: normal;
+}
+
+blockquote:before,
+blockquote:after,
+q:before,
+q:after {
+  content: "";
+}
+
+blockquote,
+q {
+  quotes: "" "";
+}
+
+h1,
+h2,
+h3,
+h4 {
+  font-family: "Fira Sans Extra Condensed", "sans-serif";
+  font-style: bold;
+}
+
+h1 {
+  color: #222;
+  font-feature-settings: "clig" off, "liga" off;
+  font-size: 56px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 66px; /* 117.857% */
+}
+
+.app {
+  background: orange;
+}
+
+/*# sourceMappingURL=index.cs.map */
