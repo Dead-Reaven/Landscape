7170260aff230b2b9e5fbfc214c2137e502a9fe6
Illia <ilyusha.drozd.2016@gmail.com>
2023-08-26 17:11:03 +0300

    fixed index.js services

--- src/js/index.js
+++ src/js/index.js
@@ -1,42 +1,22 @@
-document.addEventListener('DOMContentLoaded', function () {
-	const bulletItems = document.querySelectorAll('.pagination div')
+const toggleBurger = () => {
+	const menu = document.querySelector('#menu')
+	menu.style.display = menu.style.display === 'flex' ? '' : 'flex'
 
-	bulletItems.forEach((item) => {
-		item.addEventListener('click', function () {
-			bulletItems.forEach((otherItem) => {
-				otherItem.classList.remove('active-bullet')
-			})
-			this.classList.add('active-bullet')
-		})
-	})
+	// Добавьте следующие строки для скрытия кнопки "Замовити послугу" при открытии бургер-меню
+	const headerTopButton = document.querySelector('#header-top-button')
+	if (menu.style.display == 'flex') headerTopButton.style.display = 'none'
+	else headerTopButton.removeAttribute('style')
+}
 
-	const toggleBurger = () => {
-		const menu = document.querySelector('#menu')
-		menu.style.display = menu.style.display === 'flex' ? '' : 'flex'
+document.querySelector('#burger-button').addEventListener('click', toggleBurger)
 
-		// Добавьте следующие строки для скрытия кнопки "Замовити послугу" при открытии бургер-меню
-		const headerTopButton = document.querySelector('#header-top-button')
-		if (menu.style.display == 'flex') headerTopButton.style.display = 'none'
-		else headerTopButton.removeAttribute('style')
-	}
+const toggleServiceCard = (card) => {
+	const content = card.querySelector('#card-content')
+	content.classList.toggle('animate-open')
+	content.classList.toggle('animate-close')
+}
 
-	document
-		.querySelector('#burger-button')
-		.addEventListener('click', toggleBurger)
-
-	const toggleServiceCard = (card) => {
-		const content = card.querySelector('#card-content')
-		content.classList.toggle('animate-open')
-		content.classList.toggle('animate-close')
-	}
-	// write here code here ...
-	const serviceCards = document.querySelectorAll('.service-card')
-	console.log(serviceCards)
-	serviceCards.forEach((card) => {
-		card.addEventListener('mouseenter', () => toggleServiceCard(card))
-		card.addEventListener('mouseleave', () => toggleServiceCard(card))
-		card.addEventListener('click', () => toggleServiceCard(card))
-
-		// card.addEventListener('', toggleServiceCard)
-	})
+const serviceCards = document.querySelectorAll('.service-card')
+serviceCards.forEach((card) => {
+	card.addEventListener('click', () => toggleServiceCard(card))
 })
