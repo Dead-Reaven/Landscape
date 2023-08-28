2f28a9cf634c8e1037e84e7bc28bc1c9f7c9f6db
Illia <ilyusha.drozd.2016@gmail.com>
2023-08-28 11:28:09 +0300

    fixed services section

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,6 @@ const toggleBurger = () => {
 	const menu = document.querySelector('#menu')
 	menu.style.display = menu.style.display === 'flex' ? '' : 'flex'
 
-	// Добавьте следующие строки для скрытия кнопки "Замовити послугу" при открытии бургер-меню
 	const headerTopButton = document.querySelector('#header-top-button')
 	if (menu.style.display == 'flex') headerTopButton.style.display = 'none'
 	else headerTopButton.removeAttribute('style')
@@ -10,13 +9,41 @@ const toggleBurger = () => {
 
 document.querySelector('#burger-button').addEventListener('click', toggleBurger)
 
-const toggleServiceCard = (card) => {
+//!!! service cards
+// Function to toggle the service card's state
+const toggleOpen = (card) => {
 	const content = card.querySelector('#card-content')
-	content.classList.toggle('animate-open')
-	content.classList.toggle('animate-close')
+	content.classList.add('animate-open')
+	content.classList.remove('animate-close')
+}
+const toggleClose = (card) => {
+	const content = card.querySelector('#card-content')
+	content.classList.add('animate-close')
+	content.classList.remove('animate-open')
 }
 
+// Get all service cards
 const serviceCards = document.querySelectorAll('.service-card')
-serviceCards.forEach((card) => {
-	card.addEventListener('click', () => toggleServiceCard(card))
+
+// Add click event listeners to each service card
+let expanded = serviceCards[3]
+toggleOpen(expanded)
+
+serviceCards.forEach((card, idx) => {
+	card.addEventListener('click', () => {
+		if (card !== expanded) {
+			toggleClose(expanded)
+			if (window.innerWidth >= 1280) {
+				// Get the common parent node (assuming cards are direct children of the same parent)
+				const parent = card.parentNode
+
+				// Swap the positions of the cards using insertBefore
+				parent.insertBefore(card, expanded)
+				parent.insertBefore(expanded, card)
+			}
+			// Update the expanded card reference
+			expanded = card
+			toggleOpen(expanded)
+		}
+	})
 })
