e6768f53d7cc04f833003491f893556c95cf0f26
Illia <ilyusha.drozd.2016@gmail.com>
2023-10-11 17:37:12 +0300

    Updates

---
+++ index.98d93fe7.js
@@ -0,0 +1,2 @@
+document.querySelector("#burger-button").addEventListener("click",()=>{let e=document.querySelector("#menu");e.style.display="flex"===e.style.display?"":"flex";let t=document.querySelector("#header-top-button");"flex"==e.style.display?t.style.display="none":t.removeAttribute("style")});
+//# sourceMappingURL=index.98d93fe7.js.map
